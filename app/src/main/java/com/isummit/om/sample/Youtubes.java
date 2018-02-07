86aa135484831a093cbc7ca67c0cf353210730dd
jagan241296 <jagan241296@gmail.com>
2018-02-07 15:34:10 +0530

    guest count

--- app/src/main/java/com/isummit/om/sample/Youtubes.java
+++ app/src/main/java/com/isummit/om/sample/Youtubes.java
@@ -9,29 +9,45 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class Youtubes extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
     private YouTubePlayerFragment playerFragment;
     private YouTubePlayer mPlayer;
-    private String YouTubeKey = "AIzaSyAGXHr92fQoyIRckzQ1IxYDwEMypLQMERI";
-    Button thisevent;
-    Button prev;
-    Button abt;
-    private FirebaseAuth auth;
-
+    private String YouTubeKey = "";
+    private DatabaseReference rootRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtubes);
 
-        playerFragment =
-                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_player_fragment);
+        rootRef= FirebaseDatabase.getInstance().getReference("youtubeKey");
+
+        rootRef.orderByKey().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                YouTubeKey=dataSnapshot.getValue().toString();
+                setKey();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void setKey()
+    {
+        DatabaseReference.goOffline();
+        playerFragment =(YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_player_fragment);
 
         playerFragment.initialize(YouTubeKey, this);
-
-
     }
+
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
                                         boolean wasRestored) {
         mPlayer = player;
@@ -54,8 +70,6 @@ public class Youtubes extends AppCompatActivity implements YouTubePlayer.OnIniti
             mPlayer.play();
         }
     }
-
-
     public void onInitializationFailure(YouTubePlayer.Provider provider,
                                         YouTubeInitializationResult errorReason) {
         mPlayer = null;
