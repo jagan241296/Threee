815dfe9a3ddb851deee5989ce177f5707e7e2923
CHANDEL <sanchan3377@gmail.com>
2018-02-07 16:32:28 +0530

    [UI] Color Changes Complete

--- app/src/main/java/com/isummit/om/sample/GuestInfoDialogActivity.java
+++ app/src/main/java/com/isummit/om/sample/GuestInfoDialogActivity.java
@@ -3,8 +3,11 @@ package com.isummit.om.sample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,7 +17,7 @@ import com.squareup.picasso.Picasso;
 public class GuestInfoDialogActivity extends Activity {
 
     private TextView tv_gname, tv_desig, tv_profile, tv_company, txtview;
-    private Button bt_status;
+    private CardView bt_status;
     private ImageView img2,imgviews;
 
     @Override
@@ -50,7 +53,7 @@ public class GuestInfoDialogActivity extends Activity {
 
         Picasso.with(getApplicationContext()).load(record_spilt[4]).into(img2);
         tv_gname.setText(record_spilt[0]);
-        tv_desig.setText(record_spilt[1]);
+        tv_desig.setText("( "+record_spilt[1]+" )");
         tv_company.setText(record_spilt[2]);
         tv_profile.setText(record_spilt[3]);
 
@@ -62,7 +65,23 @@ public class GuestInfoDialogActivity extends Activity {
 
     }
     public void closeDialog(View view)
-    {
-        finish();
+    {Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        view.startAnimation(myAnim);
+        myAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
 }
